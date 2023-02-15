a8065b36507256e939265012e85893dd658d98fb
jollySleeper <git@pm21.anonaddy.com>
2023-02-15 14:42:48 +0530

    Added Script for Opening NeoVim after Building and Not Installing

---
+++ openNeovim.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+DIR=$(pwd)
+BUILD_LOCATION=$HOME/neovim-stable
+
+if [[ $* == "" ]]; then
+    ARGS=""
+elif [[ $* == "." ]]; then
+    ARGS=$DIR   
+else
+    ARGS=$DIR
+    ARGS+="/"
+    ARGS+=$*
+fi
+
+# cd $BUILD_LOCATION
+# VIMRUNTIME=runtime ./build/bin/nvim "$ARGS"
+#
+# #Go Back to Directory
+# cd $DIR
