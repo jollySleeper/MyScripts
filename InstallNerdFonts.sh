6050deb50be15184a191d4c8bcd6d010d4e719b7
jollySleeper <git@pm21.anonaddy.com>
2023-02-20 10:56:17 +0530

    Added Script to Automate Installation of NerdFonts

---
+++ InstallNerdFonts.sh
@@ -0,0 +1,46 @@
+#!/bin/bash
+
+echo "Installing DejaVuSansMono & Hack Nerd Fonts & NotoColor Emoji Font"
+
+if [[ ! -d "$HOME/.local/share/fonts/test" ]]; then
+    echo "Making Directory '$HOME/.local/share/fonts/test'"
+    mkdir -p "$HOME/.local/share/fonts/test" 
+fi
+cd "$HOME/.local/share/fonts/test" 
+
+# NerFonts 
+NERFONTS=("DejaVuSansMono" "Hack")
+NERFONT_URL="https://github.com/ryanoasis/nerd-fonts/releases/download"
+NERFONT_VERSION="v2.3.3"
+
+for i in "${NERFONTS[@]}"
+do
+    if [[ ! -d "$i-NerdFont" ]]; then
+        mkdir "$i-NerdFont"
+    fi
+    cd "$i-NerdFont"
+    rm * 2> /dev/null || true
+    echo "Downloading $i NerdFont of Version $NERFONT_VERSION"
+    wget "$NERFONT_URL/$NERFONT_VERSION/$i.zip" -q --show-progress
+    if [[ -f "$i.zip" ]]; then
+        unzip -q "$i.zip"
+        rm *Windows* *.txt *.md *.zip 2> /dev/null || true
+    fi
+    cd ..
+done
+
+# Emoji Font
+if [[ ! -d "NotoColor-EmojiFont" ]]; then
+    mkdir "NotoColor-EmojiFont"
+fi
+cd "NotoColor-EmojiFont"
+rm * 2> /dev/null || true
+echo "Downloading Noto Color Font for Emoji"
+wget "https://fonts.google.com/download?family=Noto%20Color%20Emoji" -O "NotoColorEmoji.zip" -q --show-progress
+if [[ -f "NotoColorEmoji.zip" ]]; then
+    unzip -q "NotoColorEmoji.zip"
+    rm *Windows* *.txt *.md *.zip 2> /dev/null || true
+fi
+cd ..
+
+echo "Done :P"
