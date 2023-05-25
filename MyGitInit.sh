0ced6e7697108fd12e12cc34959a9ed80c72135d
jollySleeper <git@pm21.anonaddy.com>
2023-05-25 22:19:10 +0530

    Updated: MyGitInit.sh: Refactored and Handled Case

--- MyGitInit.sh
+++ MyGitInit.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-# Initializing Git Repo And Syncing it to GitLab & Github Both
+# Initializing Git Repo And Syncing it to GitLab, GitHub & CodeBerg
 
 # Local Variables
 USER=jollySleeper
@@ -9,9 +9,15 @@ GITHUB_SSH_ALIAS=github.com
 GITLAB_SSH_ALIAS=gitlab.com
 CODEBERG_SSH_ALIAS=codeberg.org
 
+if [[ -z $* ]]; then
+    echo "Folder Name Not Provided, Exiting"
+    exit
+fi
+
+REPO=$*
 # Creating Directory
-mkdir $*
-cd $*
+mkdir -p $REPO
+cd $REPO
 
 # Git
 git init --initial-branch main
@@ -20,18 +26,18 @@ git config --local user.email $EMAIL
 
 # Making First Commit
 echo -e "\n--- \t Commiting \t ---\n"
-echo "# $* Initialized" >> README.md
+echo "# $REPO Initialized" >> README.md
 git add .
-git commit -m "Initial Commit"
+git commit -m "Repo Initialized"
 
 # Adding Origin
 # Make sure repo is created with same name on Github
-git remote add origin git@$GITHUB_SSH_ALIAS:$USER/$*.git
+git remote add origin "git@${GITHUB_SSH_ALIAS}:${USER}/${REPO}.git"
 
 # Setting Origin Push Urls
-git remote set-url --add --push origin git@$GITHUB_SSH_ALIAS:$USER/$*.git 
-git remote set-url --add --push origin git@$GITLAB_SSH_ALIAS:$USER/$*.git 
-git remote set-url --add --push origin git@$CODEBERG_SSH_ALIAS:$USER/$*.git 
+git remote set-url --add --push origin "git@${GITHUB_SSH_ALIAS}:${USER}/${REPO}.git"
+git remote set-url --add --push origin "git@${GITLAB_SSH_ALIAS}:${USER}/${REPO}.git"
+git remote set-url --add --push origin "git@${CODEBERG_SSH_ALIAS}:${USER}/${REPO}.git"
 
 # Pushing
 echo -e "\n--- \t Pushing \t ---\n"
