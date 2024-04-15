21dffb10898639bb580340b443b085df63f7dc4b
jollySleeper <git@pm21.anonaddy.com>
2024-04-15 22:49:24 +0530

    Added: AddFireWallTrafficRule: Automating FireWall Rules for Router

---
+++ Router/AddFireWallTrafficRule.sh
@@ -0,0 +1,34 @@
+#!/bin/sh
+
+# cat AddFireWallTrafficRule.sh | ssh root@router.lan|router sh 
+
+# Getting Highest FireWall Rule Index
+MaxFirewallRuleIndex=$(uci show firewall | grep -o "rule\[[0-9]*\]\=rule" | cut -d "[" -f 2 | cut -d "]" -f 1 | sort -nr | head -n 1);
+if [[ -z ${MaxFirewallRuleIndex} ]]; then
+    echo "Highest Firewall Rule Not Found. Exiting";
+    exit 1;
+fi
+echo "Found Highest Firewall Rule Index = $MaxFirewallRuleIndex"
+NewFirewallRuleIndex=$((MaxFirewallRuleIndex+1))
+echo "Using New Firewall Rule Index = $NewFirewallRuleIndex"
+
+reloadAndCommit() {
+    echo "Reloading";
+    fw3 reload &>/dev/null;
+    echo "Commiting";
+    uci commit;
+}
+
+echo "Adding Firewall Traffic Rule"
+uci add firewall rule
+# Name For FireWall Rule
+uci set firewall.@rule[$NewFirewallRuleIndex].name="BlockNest"
+# Source from Where the Device is to Blocked
+uci set firewall.@rule[$NewFirewallRuleIndex].src="lan"
+# Source IP Address
+uci set firewall.@rule[$NewFirewallRuleIndex].src_ip="192.168.1.201"
+# Block Internet
+uci set firewall.@rule[$NewFirewallRuleIndex].dest="wan"
+# DROP vs REJECT
+uci set firewall.@rule[$NewFirewallRuleIndex].target="REJECT"
+reloadAndCommit
