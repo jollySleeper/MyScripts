b5b24a87fb275cded6b3b9cb69399252893cb04b
jollySleeper <git@pm21.anonaddy.com>
2024-04-15 22:50:10 +0530

    Added: NestFirewallZoneAutomation: Enabling/Disabling Firewall Rule

---
+++ Router/NestFirewallZoneAutomation.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+# cat NestFirewallZoneAutomation.sh | ssh root@router.lan|router sh 
+
+# Having Firewall Zone as "Nest"
+FirewallZoneRuleNumber=$(uci show firewall | grep "Nest" | cut -d "[" -f 2 | cut -d "]" -f 1);
+if [[ -z ${FirewallZoneRuleNumber} ]]; then
+    echo "No Firewall Zone Rule named 'Nest' Found. Exiting";
+    exit;
+fi
+
+reloadAndCommit() {
+    echo "Reloading";
+    fw3 reload &>/dev/null;
+    echo "Commiting";
+    uci commit;
+}
+
+RuleStatus=$(echo -n "$(uci -q get firewall.@rule[$FirewallZoneRuleNumber].enabled)")
+if [[ -z $RuleStatus ]] || [[ $RuleStatus == "1" ]]; then
+    echo "Disabling Firewall Zone Rule 'Nest'";
+    uci set firewall.@rule[$FirewallZoneRuleNumber].enabled=0;
+    reloadAndCommit;
+
+    echo "Sleeping for 15s"
+    sleep 15;
+
+    echo "Enabling Firewall Zone Rule 'Nest'";
+    uci set firewall.@rule[$FirewallZoneRuleNumber].enabled=1;
+    reloadAndCommit;
+else
+    echo "Firewall Zone Rule 'Nest' already Disabled";
+fi
