346d7e026ad2942ce421c3163135607755b93de2
jollySleeper <git@pm21.anonaddy.com>
2024-07-13 19:16:54 +0530

    Updated: themeSwitcher: Added Bat Automation

--- themeSwitcher.sh
+++ themeSwitcher.sh
@@ -6,13 +6,18 @@
 
 ALACRITTY_PATH="$HOME/Dotfiles/alacritty/.config/alacritty/"
 NVIM_THEME_PATH="$HOME/Dotfiles/neovim/.config/nvim/after/plugin/theme.lua"
+BAT_CONFIG_FILE="$HOME/Dotfiles/bat/.config/bat/config"
 
 if [[ "$*" == "light" ]]; then
     COLOR="$*"
     OPST_COLOR="dark"
+    BAT_THEME="GitHub"
+    BAT_THEME_TO_REPLACE="1337"
 else
     COLOR="dark"
     OPST_COLOR="light"
+    BAT_THEME="1337"
+    BAT_THEME_TO_REPLACE="GitHub"
 fi
 
 # Alacritty
@@ -22,3 +27,9 @@ ln -s "$COLOR.toml" colors.toml
 
 # Nvim
 sed -i "21s/$OPST_COLOR/$COLOR/g" $NVIM_THEME_PATH
+
+# Bat
+for line in $(rg "theme=" $BAT_CONFIG_FILE -n -m 1 | choose -f ":" 0)
+do 
+    sed -i "${line}s/${BAT_THEME_TO_REPLACE}/${BAT_THEME}/g" $BAT_CONFIG_FILE
+done
