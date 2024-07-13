9c0d19eb889c353c322c825eb403d1c3d1920a8a
jollySleeper <git@pm21.anonaddy.com>
2024-07-13 19:11:55 +0530

    Updated: AddFireWallTrafficRule: Added Another Firewall

--- Router/AddFireWallTrafficRule.sh
+++ Router/AddFireWallTrafficRule.sh
@@ -2,16 +2,6 @@
 
 # cat AddFireWallTrafficRule.sh | ssh root@router.lan|router sh 
 
-# Getting Highest FireWall Rule Index
-MaxFirewallRuleIndex=$(uci show firewall | grep -o "rule\[[0-9]*\]\=rule" | cut -d "[" -f 2 | cut -d "]" -f 1 | sort -nr | head -n 1);
-if [[ -z ${MaxFirewallRuleIndex} ]]; then
-    echo "Highest Firewall Rule Not Found. Exiting";
-    exit 1;
-fi
-echo "Found Highest Firewall Rule Index = $MaxFirewallRuleIndex"
-NewFirewallRuleIndex=$((MaxFirewallRuleIndex+1))
-echo "Using New Firewall Rule Index = $NewFirewallRuleIndex"
-
 reloadAndCommit() {
     echo "Reloading";
     fw3 reload &>/dev/null;
@@ -19,7 +9,24 @@ reloadAndCommit() {
     uci commit;
 }
 
+# Getting Highest FireWall Rule Index
+getMaxFirewallRuleIndex() {
+    MaxFirewallRuleIndex=$(uci show firewall | grep -o "rule\[[0-9]*\]\=rule" | cut -d "[" -f 2 | cut -d "]" -f 1 | sort -nr | head -n 1);
+    if [[ -z ${MaxFirewallRuleIndex} ]]; then
+        echo "Highest Firewall Rule Not Found. Exiting";
+        exit 1;
+    fi
+    echo "Found Highest Firewall Rule Index = $MaxFirewallRuleIndex"
+}
+
+getNewFirewallRuleIndex() {
+    getMaxFirewallRuleIndex
+    NewFirewallRuleIndex=$(($MaxFirewallRuleIndex+1))
+    echo "Using New Firewall Rule Index = $NewFirewallRuleIndex"
+}
+
 echo "Adding Firewall Traffic Rule"
+getNewFirewallRuleIndex
 uci add firewall rule
 # Name For FireWall Rule
 uci set firewall.@rule[$NewFirewallRuleIndex].name="BlockNest"
@@ -27,6 +34,23 @@ uci set firewall.@rule[$NewFirewallRuleIndex].name="BlockNest"
 uci set firewall.@rule[$NewFirewallRuleIndex].src="lan"
 # Source IP Address
 uci set firewall.@rule[$NewFirewallRuleIndex].src_ip="192.168.1.201"
+# Block Internet
+uci set firewall.@rule[$NewFirewallRuleIndex].dest="wan"
+# DROP vs REJECT
+uci set firewall.@rule[$NewFirewallRuleIndex].target="REJECT"
+reloadAndCommit
+
+sleep 3
+
+echo "Adding Firewall Traffic Rule"
+getNewFirewallRuleIndex
+uci add firewall rule
+# Name For FireWall Rule
+uci set firewall.@rule[$NewFirewallRuleIndex].name="CutAcessWithoutAGH"
+# Source from Where the Device is to Blocked
+uci set firewall.@rule[$NewFirewallRuleIndex].src="lan"
+# Source IP Address
+uci set firewall.@rule[$NewFirewallRuleIndex].src_ip="192.168.1.111"
 # Block Internet
 uci set firewall.@rule[$NewFirewallRuleIndex].dest="wan"
 # DROP vs REJECT
